32041790e192ae244d319fd3a91cf07b01e10a7e
KevvKo <kklein915@gmail.com>
2022-04-24 22:57:25 +0200

    setup repository

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: 
+	g++ main.cpp -o tetris -I include -L lib
---
+++ canvas.h
@@ -0,0 +1,13 @@
+#ifndef _CANVAS_ 
+#define _CANVAS_
+
+#include <SDL2/SDL.h>
+class Canvas {
+
+public: 
+
+	Canvas			();
+	
+};
+
+#endif 
